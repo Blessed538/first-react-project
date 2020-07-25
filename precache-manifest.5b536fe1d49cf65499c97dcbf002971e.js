d149c09dca67f595191a2ae4907e7492b354a7e0
unknown <blesseddominic98@gmail.com>
2020-07-25 11:32:30 +0100

    Updates

---
+++ precache-manifest.5b536fe1d49cf65499c97dcbf002971e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "898551b68ed3782f66a499c9e93e18b1",
+    "url": "/first-react-project/index.html"
+  },
+  {
+    "revision": "3aa79b9a0693bbb742bf",
+    "url": "/first-react-project/static/css/main.c51224a4.chunk.css"
+  },
+  {
+    "revision": "02414aff6bab3a7dfcff",
+    "url": "/first-react-project/static/js/2.3d11c848.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first-react-project/static/js/2.3d11c848.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aa79b9a0693bbb742bf",
+    "url": "/first-react-project/static/js/main.f1170226.chunk.js"
+  },
+  {
+    "revision": "1e25139c0267d36e5483",
+    "url": "/first-react-project/static/js/runtime-main.326d6399.js"
+  }
+]);
